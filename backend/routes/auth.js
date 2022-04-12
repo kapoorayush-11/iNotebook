15f0f5340b513db42ecb416fbe8e7574c1be4918
Ayush <kapoorayush3127@gmail.com>
2022-04-12 16:10:36 +0530

    50 created a login end point which is working.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -19,6 +19,7 @@ router.post( '/createuser',[
   body('email','Not a valid Email format').isEmail(), 
   body('password', 'Password must be atleast 5 character').isLength({ min: 5 }),
 ], async(req, res) =>  {
+  //if there are errors, return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });  
@@ -63,5 +64,48 @@ const secPass = await bcrypt.hash(req.body.password, salt);
 
  // Earlier we used get but get use karenge toh system ke logs mei detail share ho jaati. for eg password are something jo hum nahi chahte dikhe kissi ko, so better privacy option is post.
 // req-request, res-response
+
+
+// Authenticate a user using : POST "/api/auth/login". No login required
+router.post( '/login',[
+  // If there are errors, return bad request and the errors.
+  body('email','Not a valid Email format').isEmail(), 
+  body('password','Password can not be blank').exists(), 
+], async(req, res) =>  {
+
+ //if there are errors, return bad request and the errors
+ const errors = validationResult(req);
+ if (!errors.isEmpty()) {
+   return res.status(400).json({ errors: errors.array() });  
+ }
+
+const {email, password } = req.body;
+
+try {
+  let user = await User.findOne({email});
+  if(!user){
+    return res.status(400).json({error:"Please enter correct credentials"});
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if(!passwordCompare){
+    return res.status(400).json({error:"Please enter correct credentials"});
+  }
+
+  const data = {
+    user:{
+      id:user.id
+    }
+   }
+
+  const authtoken = jwt.sign(data, JWT_SECRET);
+  res.json({authtoken})
+
+} catch (error) {
+  console.error(error.message);
+  res.status(500).send("Internal server error");
+    }
+
+})
 
 module.exports = router
