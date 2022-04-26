d8d07a90309747b4bf5a230f916bb8d079d609cb
Ayush <kapoorayush3127@gmail.com>
2022-04-26 16:50:11 +0530

    70 - Adding signup component to inotebook

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -21,10 +21,11 @@ router.post( "/createuser", [
     body("password", "Password must be atleast 5 character").isLength({min: 5,}),
   ],
   async (req, res) => {
+    let success = false;
     //if there are errors, return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ success, errors: errors.array() });
     }
     // res.send(req.body);
     // console.log(req.body)
@@ -34,9 +35,7 @@ router.post( "/createuser", [
       let user = await User.findOne({ email: req.body.email });
       // console.log(user)
       if (user) {
-        return res
-          .status(400)
-          .json({ error: "Sorry a user with this email aready exists" });
+        return res.status(400).json({success, error: "Sorry a user with this email aready exists" });
       }
 
       const salt = await bcrypt.genSalt(10);
@@ -55,8 +54,9 @@ router.post( "/createuser", [
       };
 
       const authtoken = jwt.sign(data, JWT_SECRET);
+      success = true;
       // res.json(user);
-      res.json({ authtoken });
+      res.json({success, authtoken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some Error Occured yaar");
